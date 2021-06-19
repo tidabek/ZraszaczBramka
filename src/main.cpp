a172d306afd463c593d1a3b43bfb200ad0c0d141
tidabek <tidabek@users.noreply.github.com>
2021-06-19 10:35:58 +0200

    Dodany

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,57 @@
 // https://opensource.org/licenses/MIT
 
 #include <Arduino.h>
+#include <stdio.h>
 
+#define TYPE "ZRASZACZ"       //rodzaj urzadzenia
+#define SW "1.0"              //wersja programu
+#define HW "Nano3"            //wersja sprzetowa
+
+#define LEDBLINK_PIN 13       // I/O pin connected to the LED.
+#define LEDBLINK_MS 1000      // Blink rate (in milliseconds).
 
 int pompa = 6;                                  // Pin do przekaznika
-int dioda = 13;                                 // Dioda testowa
+//int dioda = 13;                                 // Dioda testowa
 int czujnik_r1 = 5;                             // Pin odczytu czujnika ruchu przy wjezdzie
 int czujnik_r2 = 4;                             // Pin odczytu czujnika wyjazdu 2
 unsigned long odpalenie_pompy = 0;
-unsigned long czas_pracy_pompy = 8 * 1000;
+unsigned long czas_pracy_pompy = 5 * 1000;
 
 boolean czujnik_r1_state = false;
 boolean czujnik_r2_state = false;
 int sekwencja = 0;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("TYP: " + String(TYPE));
+  Serial.println(" HW: " + String(HW));
+  Serial.println(" SW: " + String(SW));
   pinMode(czujnik_r1, INPUT);
   pinMode(czujnik_r2, INPUT);
   pinMode(pompa, OUTPUT);
-  pinMode(dioda, OUTPUT);
+  //pinMode(dioda, OUTPUT);
 }
+
+void ledBlink()
+{
+  static unsigned int ledStatus = LOW;   // Last set LED mode.
+  static unsigned long ledBlinkTime = 0; // LED blink time.
+
+  // LED blinking heartbeat. Yes, we are alive.
+  // For explanation, see:
+  // http://playground.arduino.cc/Code/TimingRollover
+  if ((long)(millis() - ledBlinkTime) >= 0)
+  {
+    // Toggle LED.
+    ledStatus = (ledStatus == HIGH ? LOW : HIGH);
+
+    // Set LED pin status.
+    digitalWrite(LEDBLINK_PIN, ledStatus);
+
+    // Reset "next time to toggle" time.
+    ledBlinkTime = millis() + LEDBLINK_MS;
+  }
+} // End of ledBlink()
 
 void set_pompa(){
   digitalWrite(pompa,HIGH);
@@ -42,6 +73,7 @@ void reset_pompa(){
 }
 
 void loop() {
+  ledBlink(); // Blink the LED to let the user know we are still alive.
   // test czujnika r1
   if(digitalRead(czujnik_r1) == LOW){
     delay(10);
